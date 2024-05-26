92b03c72839fee5642a0cf5efb06ab5eb0cb02fb
IMatecocido <camilotrep@gmail.com>
2024-05-26 16:51:25 -0300

    modulo 5
    asdf

--- halooo/Assets/Scenes/script/Holamundo.cs
+++ halooo/Assets/Scenes/script/Holamundo.cs
@@ -1,22 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 public class Holamundo : MonoBehaviour
 {
+    int x;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Algo paso");
+        x = 0;
         Debug.LogError("Algo salio muy mal");
         Debug.LogWarning("algo salio medianamente mal");
         print("algo salio mal");
-
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        x = x + 1;
+        Debug.Log(x);
+
     }
 }
