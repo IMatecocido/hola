64c20a42fac74bce8a36ef118761804be3749db8
IMatecocido <camilotrep@gmail.com>
2024-05-25 21:57:45 -0300

    Hola mundo
    Victoria al lograrlo

---
+++ halooo/Assets/Scenes/script/Holamundo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holamundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Algo paso");
+        Debug.LogError("Algo salio muy mal");
+        Debug.LogWarning("algo salio medianamente mal");
+        print("algo salio mal");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
